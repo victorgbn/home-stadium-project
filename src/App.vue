dfc9768766c52ef7cabf18423c64d171f56e1baa
victorgbn <49316233+victorgbn@users.noreply.github.com>
2021-03-04 11:58:49 +0100

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    {{data}}
+      <div class="">
+
+      </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'App',
+  components: {
+
+  },
+  data () {
+    return {
+      data: null, 
+      team1: null, 
+      team
+    }
+  },
+  mounted () {
+    axios
+      .get('api.json')
+      .then(response => (this.data = response))
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
